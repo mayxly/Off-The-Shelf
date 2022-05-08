d71ef967eca91af8b88b0a3fe59f976926d323c9
mayxly <may.ly7303@gmail.com>
2022-05-08 15:52:58 -0400

    Code Screen component to mimic SafeAreaView.

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Platform, StatusBar } from 'react-native';
+import { FlatList, StyleSheet, Platform, StatusBar } from 'react-native';
 
 import ListItem from '../components/ListItem';
+import Screen from '../components/Screen';
 
 const messages = [
     {   id: 1,
@@ -19,7 +20,7 @@ const messages = [
 
 function MessagesScreen(props) {
     return (
-        <SafeAreaView style={styles.screen}>
+        <Screen>
             <FlatList 
                 data={messages}
                 keyExtractor={message => message.id.toString()}
@@ -31,14 +32,12 @@ function MessagesScreen(props) {
                     /> 
                 } 
             />
-        </SafeAreaView>
+        </Screen>
     );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-    }
+ 
 })
 
 export default MessagesScreen;
