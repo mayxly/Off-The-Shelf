b927f335aad370496525e18d78066357c324d2dd
mayxly <may.ly7303@gmail.com>
2022-05-08 17:48:15 -0400

    Create reusable icon component.

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { View } from "react-native";
-
 import AppText from './app/components/AppText';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
   return (
-    <ListingDetailsScreen/>
+    <Screen>
+      <Icon
+        name="email"
+        size={50}
+        backgroundColor="red"
+        iconColor="white"/>
+    </Screen>
   );
 }
