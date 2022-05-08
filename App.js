76df6643719ef214300ffe3b1ab6717a3fa4b7ba
mayxly <may.ly7303@gmail.com>
2022-05-08 17:57:49 -0400

    Fix ListItem to use for account screen.

--- App.js
+++ App.js
@@ -9,15 +9,14 @@ import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
+import ListItem from './app/components/ListItem';
 
 export default function App() {
   return (
     <Screen>
-      <Icon
-        name="email"
-        size={50}
-        backgroundColor="red"
-        iconColor="white"/>
+      <ListItem 
+        title="My title"  
+        ImageComponent={<Icon name="email"/>}/>
     </Screen>
   );
 }
