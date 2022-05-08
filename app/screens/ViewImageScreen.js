6c085a42038521dd52b8c98218f694bc9cfea34f
mayxly <may.ly7303@gmail.com>
2022-05-08 15:20:45 -0400

    Add close and trash icon in ViewImgScreen.

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import colours from '../config/colours';
 
 function ViewImageScreen(props) {
     return (
         
         <View style={styles.container}>
-        <View style={styles.closeIcon}></View>
-        <View style={styles.deleteIcon}></View>
+        <View style={styles.closeIcon}>
+            <MaterialCommunityIcons name="close" color="white" size={30}/>
+        </View>
+        <View style={styles.deleteIcon}>
+            <MaterialCommunityIcons name="trash-can-outline" color="white" size={30}/>
+        </View>
             <Image 
                 resizeMode="contain"
                 style={styles.image} 
-                source={require("../assets/stoolbooks.jpg")}/>
+                source={require("../assets/SoulBook.jpg")}/>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     closeIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: "#fc5c65",
         position: "absolute",
         top: 40,
         left: 30
@@ -30,9 +32,6 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     deleteIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: "#fff",
         position: "absolute",
         top: 40,
         right: 30
